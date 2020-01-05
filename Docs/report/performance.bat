0ca1dfe9f2a0dfdf2505b21aa6290b98f0a4a5f9
Jan Skála <skalaja7@fel.cvut.cz>
2020-01-05 18:49:30 +0100

    Edit performance script.

---
+++ Docs/report/performance.bat
@@ -0,0 +1,8 @@
+GPU.exe -t iris_train.csv -e iris_test.csv > iris1.txt
+GPU.exe -e iris_train.csv -t iris_test3.csv > iris2.txt
+GPU.exe -t mnist_train1.csv -e mnist_test1.csv > mnist1.txt
+GPU.exe -e mnist_train1.csv -t mnist_test1.csv > mnist2.txt
+GPU.exe -t mnist_train2.csv -e mnist_test2.csv > mnist3.txt
+GPU.exe -e mnist_train2.csv -t mnist_test2.csv > mnist4.txt
+GPU.exe -t mnist_train3.csv -e mnist_test3.csv > mnist5.txt
+GPU.exe -e mnist_train3.csv -t mnist_test3.csv > mnist6.txt
