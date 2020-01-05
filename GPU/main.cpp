967c966003491a854ca804b2b093ce97a78f78ed
Jan Skala <skalaja7@fel.cvut.cz>
2020-01-05 15:31:29 +0100

    Add time measure.

--- GPU/main.cpp
+++ GPU/main.cpp
@@ -6,12 +6,26 @@
 #include "CSV.h"
 #include "cuda_runtime.h"
 #include "default.h"
+#include <chrono>
+#include <ctime>
 
 using namespace std;
+using namespace std::chrono;
 string train, eval;
 int iterations;
 float learning_rate;
 bool verbose = false;
+
+
+void print_time(time_point<system_clock> start, time_point<system_clock> end, const string& message) {
+	duration<double> elapsed_seconds = end - start;
+	time_t end_time = system_clock::to_time_t(end);
+	cout << message << elapsed_seconds.count() << "s" << endl;
+}
+
+void print_time(time_point<system_clock> start, time_point<system_clock> end) {
+	print_time(start, end, "elapsed time: ");
+}
 
 void print(vector<float> const& input) {
 	copy(input.begin(),
@@ -20,19 +34,33 @@ void print(vector<float> const& input) {
 }
 
 void training(Perceptron* p) {
+	cout << "==============" << endl;
+	cout << "CPU Training" << endl;
+	cout << "==============" << endl;
+
 	vector<vector<float>> data;
 	vector<float> classes;
 	tie(data, classes) = CSV::parse_data_with_classes(train);
+
+	auto start = std::chrono::system_clock::now();
 	p->fit(data, classes);
+	auto end = std::chrono::system_clock::now();
+	print_time(start, end);
 }
 
 void training_gpu(Perceptron* p) {
+	cout << "==============" << endl;
+	cout << "GPU Training" << endl;
+	cout << "==============" << endl;
+
 	vector<vector<float>> data;
 	vector<float> classes;
 	tie(data, classes) = CSV::parse_data_with_classes(train);
 
 	float* g_classes;
 	gpuErrchk(cudaMalloc(&g_classes, classes.size() * sizeof(float)));
+	//copy to GPU
+	auto startCopy = std::chrono::system_clock::now();
 	gpuErrchk(cudaMemcpy(g_classes, &classes[0], classes.size() * sizeof(float), cudaMemcpyHostToDevice));
 
 	float* g_data;
@@ -44,7 +72,12 @@ void training_gpu(Perceptron* p) {
 		auto& current = data[i];
 		gpuErrchk(cudaMemcpy(g_data + i * width, &(current[0]), width * sizeof(float), cudaMemcpyHostToDevice));
 	}
+
+	auto start = std::chrono::system_clock::now();
 	p->fit_gpu(g_data, g_classes, data.size(), width);
+	auto end = std::chrono::system_clock::now();
+	print_time(start, end);
+	print_time(startCopy, end, "elapsed time with copying: ");
 }
 
 void evaluation(Perceptron* p) {
@@ -58,6 +91,7 @@ void evaluation(Perceptron* p) {
 	cout << "CPU EVALUATION" << endl;
 	cout << "==============" << endl;
 
+	auto start = std::chrono::system_clock::now();
 	for (size_t i = 0; i < data.size(); i++)
 	{
 		auto result = p->predict(data[i]);
@@ -66,6 +100,8 @@ void evaluation(Perceptron* p) {
 			cout << "Classification: " << result << " Expected: " << expected << endl;
 		if (result == expected) correct++;
 	}
+	auto end = std::chrono::system_clock::now();
+	print_time(start, end);
 
 	unsigned int wrong = data.size() - correct;
 
@@ -77,34 +113,42 @@ void evaluation(Perceptron* p) {
 }
 
 void evaluation_gpu(Perceptron* p) {
+	cout << "==============" << endl;
+	cout << "GPU EVALUATION" << endl;
+	cout << "==============" << endl;
 	vector<vector<float>> data;
 	vector<float> classes;
 	tie(data, classes) = CSV::parse_data_with_classes(eval);
 
+	// allocation GPU
 	float* g_classes;
 	gpuErrchk(cudaMalloc(&g_classes, classes.size() * sizeof(float)));
-	gpuErrchk(cudaMemcpy(g_classes, &classes[0], classes.size() * sizeof(float), cudaMemcpyHostToDevice));
-
 	float* g_data;
 	size_t pitch;
 	auto width = data[0].size();
 	cudaMallocPitch((void**)&g_data, &pitch, width * sizeof(float), data.size());
+
+	// copy to GPU
+	auto startCopy = std::chrono::system_clock::now();
+	gpuErrchk(cudaMemcpy(g_classes, &classes[0], classes.size() * sizeof(float), cudaMemcpyHostToDevice));
+
 	for (size_t i = 0; i < data.size(); i++)
 	{
 		auto& current = data[i];
 		gpuErrchk(cudaMemcpy(g_data + i * width, &(current[0]), width * sizeof(float), cudaMemcpyHostToDevice));
 	}
 
-	cout << "==============" << endl;
-	cout << "GPU EVALUATION" << endl;
-	cout << "==============" << endl;
-
+	auto start = std::chrono::system_clock::now();
 	float* result = p->predict_gpu(g_data, data.size(), width);
 
 	int correct = 0;
 
 	float* result_cpu = (float*)calloc(data.size(), sizeof(float));
 	gpuErrchk(cudaMemcpy(result_cpu, result, data.size() * sizeof(float), cudaMemcpyDeviceToHost));
+	auto end = std::chrono::system_clock::now();
+	print_time(start, end);
+	print_time(startCopy, end, "elapsed time with copying: ");
+
 
 	for (size_t i = 0; i < data.size(); i++)
 	{
@@ -112,7 +156,7 @@ void evaluation_gpu(Perceptron* p) {
 		if (verbose)
 			cout << "Classification: " << result_cpu[i] << " Expected: " << expected << endl;
 		if (result_cpu[i] == expected) correct++;
-	}	
+	}
 	int wrong = data.size() - correct;
 
 	cout << "==============" << endl;
